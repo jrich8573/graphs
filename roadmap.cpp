a0a2da66e08bfca2b04373918096745108c360b1
Jason Rich <jrich@redventures.com>
2022-04-23 16:31:30 -0400

    still going

--- roadmap.cpp
+++ roadmap.cpp
@@ -62,16 +62,15 @@ typedef int Vertex;
 typedef std::vector<Vertex> Trip;
 Trip RoadMap::planTheTrip(std::string fromCity, std::string toCity){
 
-  std::vector<std::string> c;
+  std::vector<std::string> c {};
   auto pos = cityNames.find(fromCity);
-  do{
-    if(pos == cityNames.end()){ // Never seen from city before need to add it
-      c.push_back(fromCity);
-    }else{
-      c.push_back(toCity);
-    }
-  }while(pos);
+  if(pos == cityNames.end()){ // Never seen from city before need to add it
+    c.push_back(fromCity);
+  }else{
+    c.push_back(toCity);
+  }
   
+
   return c;
 }
 
