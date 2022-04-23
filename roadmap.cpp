e82580cb1f138df1286ff9da3a09d5b3a1e65892
Jason Rich <jrich@redventures.com>
2022-04-23 15:55:50 -0400

    changed City to std::string

--- roadmap.cpp
+++ roadmap.cpp
@@ -62,7 +62,7 @@ typedef int Vertex;
 typedef std::vector<Vertex> Trip;
 Trip RoadMap::planTheTrip(std::string fromCity, std::string toCity){
 
-  std::vector<City> c;
+  std::vector<std::string> c;
   auto pos = cityNames.find(fromCity);
   if(pos == cityNames.end()){ // Never seen from city before need to add it
    c = addCity(fromCity);
