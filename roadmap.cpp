4323c105a9fd130ac3c7352e81f3afe9b498dd69
Jason Rich <jrich@redventures.com>
2022-04-23 23:13:23 -0400

    still troubleshooting

--- roadmap.cpp
+++ roadmap.cpp
@@ -62,7 +62,8 @@ typedef int Vertex;
 typedef std::vector<Vertex> Trip;
 RoadMap::Trip RoadMap::planTheTrip(std::string fromCity, std::string toCity){
 
-  Trip t (2);
+  Trip t;
+  t.push_back(2);
   std::vector<std::string> c;
   auto pos = cityNames.find(fromCity);
   if (pos == cityNames.end()){
