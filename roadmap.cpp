717f6fb1f8259b1fb3a2073f310cc141704ba315
Jason Rich <jrich@redventures.com>
2022-04-23 22:47:14 -0400

    still troubleshooting

--- roadmap.cpp
+++ roadmap.cpp
@@ -72,7 +72,7 @@ RoadMap::Trip RoadMap::planTheTrip(std::string fromCity, std::string toCity){
     v = add_vertex(g);
     g[v].city = toCity;
     cityNames[toCity] = v;
-    t.push_back(v);
+    t.addCity(v);
   }
   
   return t;
