31b36b5519aa320c1b3a1475ee336292ffbcbfea
Jason Rich <jrich@redventures.com>
2022-04-25 19:10:00 -0400

    still troubleshooting

--- roadmap.cpp
+++ roadmap.cpp
@@ -85,31 +85,31 @@ RoadMap::Trip RoadMap::planTheTrip(std::string fromCity, std::string toCity){
   std::vector<Vertex> weight = findWeightedShortestPath(0,0); 
 
   dist[(int)start] = 0;
-  typedef std::pair<int, Vertex> Element;
-	std::priority_queue<Element, std::vector<Element>, std::greater<Element> >
-	pq;
-	pq.push (Element(0, start));
+  //typedef std::pair<int, Vertex> Element;
+	//std::priority_queue<Element, std::vector<Element>, std::greater<Element> >
+	//pq;
+	//pq.push (Element(0, start));
 
 	// Find the shortest path
-	while (!pq.empty()){
-		Element top = pq.top();
-		pq.pop();
-		Vertex v = top.second;
-		if (v == finish) break; // exit when we reach the finish vertex
-		int d = dist[v];
-		if (top.first == d){
-			auto outgoing = out_edges(v, g);
-			for (auto e = outgoing.first; e != outgoing.second; ++e){
-				Vertex w = target(*e, g);
-				unsigned wDist = d + weight.at(*e);
-				if (dist[w] > wDist){
-				   dist[w] = wDist;
-				   pq.push(Element(wDist, w));
-				   cameFrom[w] = v;
-				}
-			}
-		}
-	}
+	//while (!pq.empty()){
+	//	Element top = pq.top();
+	//	pq.pop();
+	//	Vertex v = top.second;
+	//	if (v == finish) break; // exit when we reach the finish vertex
+	//	int d = dist[v];
+	//	if (top.first == d){
+	//		auto outgoing = out_edges(v, g);
+	//		for (auto e = outgoing.first; e != outgoing.second; ++e){
+	//			Vertex w = target(*e, g);
+	//			unsigned wDist = d + weight.at(*e);
+	//			if (dist[w] > wDist){
+	//			   dist[w] = wDist;
+	//			   pq.push(Element(wDist, w));
+	//			   cameFrom[w] = v;
+	//			}
+	//		}
+	//	}
+	//}
 
   // Extract path
 	std::vector<Vertex> path;
